3dc8d85c2fe40f74924e2908939f9d145aac5fca
Илья Грошев <Tyuiu.GroshevID@outlook.com>
2025-12-09 17:30:53 +0500

    Add project files.

---
+++ Tyuiu.GroshevID.Sprint6.Task0.V7.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.GroshevID.Sprint6.Task0.V7.Lib
+{
+    public class DataService : ISprint6Task0V7
+    {
+        public double Calculate(int x)
+        {
+            double y = (Math.Pow((double)x, 3)) / (2 * (Math.Pow((double)x + 5, 2)));
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.GroshevID.Sprint6.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GroshevID.Sprint6.Task0.V7.Lib;
+
+namespace Tyuiu.GroshevID.Sprint6.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 0.211;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
