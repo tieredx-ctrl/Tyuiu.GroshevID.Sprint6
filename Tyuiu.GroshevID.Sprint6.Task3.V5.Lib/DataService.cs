d77a6033e71886cec92aa5552cd9d577066ee2a3
Илья Грошев <Tyuiu.GroshevID@outlook.com>
2025-12-11 17:23:31 +0500

    Task3

---
+++ Tyuiu.GroshevID.Sprint6.Task3.V5.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.GroshevID.Sprint6.Task3.V5.Lib
+{
+    public class DataService : ISprint6Task3V5
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.GroshevID.Sprint6.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.GroshevID.Sprint6.Task3.V5.Lib;
+
+namespace Tyuiu.GroshevID.Sprint6.Task3.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] wait = new int[,] { };
+            int[,] matrix = new int[,] { { 30, -20, 7, -8, 9 }, { 32, 17, -14, -7, 33 }, { 19, -19, -13, 14, -20 }, { 11, 30, -1, 26, 6 }, { 30, -15, -20, -5, 15 } };
+            int[,] res = ds.Calculate(matrix);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
