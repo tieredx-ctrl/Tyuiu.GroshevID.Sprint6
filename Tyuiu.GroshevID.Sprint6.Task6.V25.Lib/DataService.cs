5c4d753c11b3df8bdbe4c43345d3b54cdb4f6c30
Илья Грошев <Tyuiu.GroshevID@outlook.com>
2025-12-11 17:53:21 +0500

    Task6

---
+++ Tyuiu.GroshevID.Sprint6.Task6.V25.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.GroshevID.Sprint6.Task6.V25.Lib
+{
+    public class DataService : ISprint6Task6V25
+    {
+        public string CollectTextFromFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.GroshevID.Sprint6.Task6.V25/MainForm.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.GroshevID.Sprint6.Task6.V25
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
